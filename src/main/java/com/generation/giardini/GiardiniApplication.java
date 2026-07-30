361ed06cd1241fa6f0844464b90861d5f334f51d
Giacomo Archetti <archettigiacomoarchetti@gmail.com>
2026-07-30 12:10:17 +0200

    CREAZIONE PROGETTO SPRING: creazione progetto spring + inizializzazione repo

---
+++ src/main/java/com/generation/giardini/GiardiniApplication.java
@@ -0,0 +1,13 @@
+package com.generation.giardini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiardiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiardiniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/giardini/GiardiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.giardini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiardiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
