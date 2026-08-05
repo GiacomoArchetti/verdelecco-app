2b7284000aa6911d58f10c7aedd5437fded0e83d
Giacomo Archetti <archettigiacomoarchetti@gmail.com>
2026-08-05 17:05:13 +0200

    fix(PrenotazioneService): si era cancellato l'ho rifatto

--- src/main/java/com/generation/giardini/service/prenotazione/PrenotazioneService.java
+++ src/main/java/com/generation/giardini/service/prenotazione/PrenotazioneService.java
@@ -1,5 +1,17 @@
 package com.generation.giardini.service.prenotazione;
 
+import java.util.List;
+
+import com.generation.giardini.dto.PrenotazioneDTO;
+
 public interface PrenotazioneService {
+
+    boolean create(PrenotazioneDTO dto);
+
+    List<PrenotazioneDTO> readAll();
+
+    PrenotazioneDTO readById(Long id);
+
+    boolean delete(Long id);
 
 }
