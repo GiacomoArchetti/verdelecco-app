15eec2ff5e577f59f69c6bfc4fcf3bf09c62bd58
Giacomo Archetti <archettigiacomoarchetti@gmail.com>
2026-08-20 16:36:18 +0200

    fix(SecurityConfig.class): piccole modifiche

--- src/main/java/com/generation/giardini/config/SecurityConfig.java
+++ src/main/java/com/generation/giardini/config/SecurityConfig.java
@@ -1,22 +1,15 @@
 package com.generation.giardini.config;
 
-import java.util.HashMap;
-import java.util.Map;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-
-import com.generation.giardini.security.CustomUserDetailsService;
 
 @Configuration
 @EnableMethodSecurity
